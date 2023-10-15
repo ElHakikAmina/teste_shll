89ebd7d6cfe54a7c404e675249893fddd17d4321
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-10-15 19:36:36 +0000

    variable non trouvé dans tou le code

--- shell.h
+++ shell.h
@@ -33,7 +33,7 @@
 /* 1 if using system getline() */
 /* change USE_GETLINE to USING_GETLINE  */
 #define USING_GETLINE 0
-#define USE_STRTOK 0
+/* #define USE_STRTOK 0 */
 
 #define HIST_FILE	".simple_shell_history"
 #define HIST_MAX	4096
