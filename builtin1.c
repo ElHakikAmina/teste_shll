49f7d76b84941b7b62010357059e5f56c9724ecf
EL HAKIK AMINA <amina.el.hakik2@gmail.com>
2023-10-16 11:25:52 +0000

    comment

--- builtin1.c
+++ builtin1.c
@@ -1,7 +1,6 @@
 #include "shell.h"
-
+/* CREATED BY Amina El Hakik, Mehdi Belaazri*/
 /**
- * created by Amina El Hakik, Mehdi Belaazri
  * _myhistoryfnc - displays the history list, one command by line, preceded
  *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
